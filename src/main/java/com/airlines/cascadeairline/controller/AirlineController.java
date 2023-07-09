3ab606d9c492f2c08cb5312f8d8d571ae421d99d
SUJITH KANNAN K H <67060938+Suji-2d@users.noreply.github.com>
2023-07-09 15:50:33 +0100

    Update AirlineController.java

--- src/main/java/com/airlines/cascadeairline/controller/AirlineController.java
+++ src/main/java/com/airlines/cascadeairline/controller/AirlineController.java
@@ -7,7 +7,6 @@ import java.util.Optional;
 
 import javax.transaction.Transactional;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
